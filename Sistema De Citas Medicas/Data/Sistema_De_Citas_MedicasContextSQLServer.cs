0129bcb2e640b638b0a2be31450853df5fc197c1
Josue Mullo <josue.mullo@udla.edu.ec>
2025-04-29 08:07:54 -0500

    Ajuste en DbContext para evitar error de múltiples cascadas al aplicar migración, y creacion de migracion

--- Sistema De Citas Medicas/Data/Sistema_De_Citas_MedicasContextSQLServer.cs
+++ Sistema De Citas Medicas/Data/Sistema_De_Citas_MedicasContextSQLServer.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Sistema_De_Citas_Medicas.Models;
 
-    public class Sistema_De_Citas_MedicasContextSQLServer : DbContext
+public class Sistema_De_Citas_MedicasContextSQLServer : DbContext
+{
+    public Sistema_De_Citas_MedicasContextSQLServer(DbContextOptions<Sistema_De_Citas_MedicasContextSQLServer> options)
+        : base(options)
     {
-        public Sistema_De_Citas_MedicasContextSQLServer (DbContextOptions<Sistema_De_Citas_MedicasContextSQLServer> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Sistema_De_Citas_Medicas.Models.Medico> Medico { get; set; } = default!;
-
-public DbSet<Sistema_De_Citas_Medicas.Models.Paciente> Paciente { get; set; } = default!;
-
-public DbSet<Sistema_De_Citas_Medicas.Models.HistorialMedico> HistorialMedico { get; set; } = default!;
-
-public DbSet<Sistema_De_Citas_Medicas.Models.Cita> Cita { get; set; } = default!;
-
-public DbSet<Sistema_De_Citas_Medicas.Models.Usuario> Usuario { get; set; } = default!;
     }
+
+    public DbSet<Medico> Medico { get; set; } = default!;
+    public DbSet<Paciente> Paciente { get; set; } = default!;
+    public DbSet<HistorialMedico> HistorialMedico { get; set; } = default!;
+    public DbSet<Cita> Cita { get; set; } = default!;
+    public DbSet<Usuario> Usuario { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Cita>()
+            .HasOne(c => c.Paciente)
+            .WithMany()
+            .HasForeignKey(c => c.PacienteId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Cita>()
+            .HasOne(c => c.Medico)
+            .WithMany()
+            .HasForeignKey(c => c.MedicoId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ Sistema De Citas Medicas/Migrations/20250429125831_PrimeraMigracionGrupo4.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sistema_De_Citas_Medicas.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeraMigracionGrupo4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreCompleto = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rol = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medico",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Especialidad = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Consultorio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medico", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medico_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Paciente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paciente", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Paciente_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cita",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaHora = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    PacienteId = table.Column<int>(type: "int", nullable: false),
+                    MedicoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cita", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cita_Medico_MedicoId",
+                        column: x => x.MedicoId,
+                        principalTable: "Medico",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Cita_Paciente_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Paciente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HistorialMedico",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Diagnostico = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tratamiento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Observaciones = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CitaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistorialMedico", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HistorialMedico_Cita_CitaId",
+                        column: x => x.CitaId,
+                        principalTable: "Cita",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cita_MedicoId",
+                table: "Cita",
+                column: "MedicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cita_PacienteId",
+                table: "Cita",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HistorialMedico_CitaId",
+                table: "HistorialMedico",
+                column: "CitaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medico_UsuarioId",
+                table: "Medico",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Paciente_UsuarioId",
+                table: "Paciente",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HistorialMedico");
+
+            migrationBuilder.DropTable(
+                name: "Cita");
+
+            migrationBuilder.DropTable(
+                name: "Medico");
+
+            migrationBuilder.DropTable(
+                name: "Paciente");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
