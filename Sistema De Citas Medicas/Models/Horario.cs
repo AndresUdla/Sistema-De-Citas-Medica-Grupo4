6f13d0729e6c08aa0f6e19bb31d8a79c665f6927
Josue Mullo <josue.mullo@udla.edu.ec>
2025-04-29 20:31:52 -0500

    Creacion de modelo Horario

---
+++ Sistema De Citas Medicas/Models/Horario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sistema_De_Citas_Medicas.Models
+{
+    public class Horario
+    {
+        [Key]
+        public int HorarioId { get; set; }
+
+        [Required]
+        public DayOfWeek Dia { get; set; } // Día de la semana (lunes, martes, etc.)
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan HoraInicio { get; set; } // Hora de inicio del turno
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan HoraFin { get; set; } // Hora de fin del turno
+
+        // Fecha de creación del horario
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        // Foreign Key hacia Medico
+        [Required]
+        public int MedicoId { get; set; }
+
+        [ForeignKey("MedicoId")]
+        public Medico Medico { get; set; }
+    }
+}
