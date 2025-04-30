875db8930bb96a337d63639163bcaaabac874bc3
Josue Mullo <josue.mullo@udla.edu.ec>
2025-04-29 20:19:24 -0500

    Creacion de atributos, anotaciones y foreing key de modelo Administrador

--- Sistema De Citas Medicas/Models/Administrador.cs
+++ Sistema De Citas Medicas/Models/Administrador.cs
@@ -1,6 +1,25 @@
-﻿namespace Sistema_De_Citas_Medicas.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sistema_De_Citas_Medicas.Models
 {
     public class Administrador
     {
+        [Key]
+        public int AdministradorId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio.")]
+        [Phone(ErrorMessage = "Debe ser un número de teléfono válido.")]
+        public string Telefono { get; set; }
+
+        [Required]
+        [ForeignKey("Usuario")]
+        public int UsuarioId { get; set; }
+
+        public Usuario Usuario { get; set; }
     }
 }
