50ac7a5a9cc777d211fc27c56816c0016e97dcf4
Josue Mullo <josue.mullo@udla.edu.ec>
2025-04-30 19:26:29 -0500

    Codigo js añadido para añadir funcioalidad de poder ver la contraseña o no

--- Sistema De Citas Medicas/wwwroot/js/site.js
+++ Sistema De Citas Medicas/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function () {
+    const togglePassword = document.getElementById('togglePassword');
+    if (togglePassword) {
+        togglePassword.addEventListener('click', function () {
+            const passwordField = document.getElementById('passwordField');
+            const passwordIcon = document.getElementById('togglePasswordIcon');
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                passwordIcon.classList.remove('bi-eye-slash');
+                passwordIcon.classList.add('bi-eye');
+            } else {
+                passwordField.type = 'password';
+                passwordIcon.classList.remove('bi-eye');
+                passwordIcon.classList.add('bi-eye-slash');
+            }
+        });
+    }
+});
